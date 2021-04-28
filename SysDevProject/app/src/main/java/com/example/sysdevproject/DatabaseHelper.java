81b8587c2456d995e3d166f48bb66bd7f28903c4
aliL123 <56936736+aliL123@users.noreply.github.com>
2021-04-28 15:07:03 -0400

    Updated Database Class

--- SysDevProject/app/src/main/java/com/example/sysdevproject/DatabaseHelper.java
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/DatabaseHelper.java
@@ -1,4 +1,29 @@
 package com.example.sysdevproject;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    private static final String DB_NAME = "Restaurant.db";
+    
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
