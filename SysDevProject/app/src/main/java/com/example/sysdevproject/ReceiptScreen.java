6ac9f365f6bf0092a79c54b93edea70f3c98be0a
BrandonN007 <brandon.nguyen2002@yahoo.com>
2021-04-30 17:11:51 -0400

    Added Receipt Screen

---
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/ReceiptScreen.java
@@ -0,0 +1,14 @@
+package com.example.sysdevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiptScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_screen);
+    }
+}
