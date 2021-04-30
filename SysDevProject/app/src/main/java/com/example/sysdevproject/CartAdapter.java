56514d1eacb69881a3ddba42a837d7978cd82f6d
Akash Singh <akash_star12@hotmail.com>
2021-04-30 17:07:02 -0400

    Added the cart recycler view

---
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/CartAdapter.java
@@ -0,0 +1,67 @@
+package com.example.sysdevproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class CartAdapter  extends RecyclerView.Adapter<CartAdapter.ViewHolder>{
+    ArrayList<String> itemNames = new ArrayList<>();
+    ArrayList<String> itemQuantities = new ArrayList<>();
+
+    Context mContext;
+
+    public CartAdapter(ArrayList<String> itemNames,
+                       ArrayList<String> itemQuantities,
+                       Context mContext) {
+
+        this.itemNames = itemNames;
+        this.itemQuantities = itemQuantities;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public CartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items, parent, false);
+        return new CartAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+
+        holder.cartItemTextView.setText(itemNames.get(position));
+        holder.cartQuantityTextView.setText(itemQuantities.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView cartItemTextView, cartQuantityTextView;
+        LinearLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+            cartItemTextView = itemView.findViewById(R.id.cartItemName);
+            cartQuantityTextView = itemView.findViewById(R.id.itemCartQuantity);
+
+            parentLayout = itemView.findViewById(R.id.parent);
+
+        }
+    }
+}
