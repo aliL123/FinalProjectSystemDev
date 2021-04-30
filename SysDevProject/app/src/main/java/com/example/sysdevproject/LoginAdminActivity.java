77f6c6c7256ec5bbe520ba4fcf60dfa4ce96815d
ParkSeung1 <42653969+ParkSeung1@users.noreply.github.com>
2021-04-30 19:03:40 -0300

    Transfer to main to adminLogin

--- SysDevProject/app/src/main/java/com/example/sysdevproject/LoginAdminActivity.java
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/LoginAdminActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginAdminActivity extends AppCompatActivity {
+public class LoginAdminActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
