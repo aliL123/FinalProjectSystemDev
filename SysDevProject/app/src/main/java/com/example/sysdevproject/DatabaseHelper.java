313626722cb0dfbc92ea290b291698463856b0fa
aliL123 <56936736+aliL123@users.noreply.github.com>
2021-04-30 15:41:52 -0400

    Created Database,  added Tables and attributes
    Created the database syntax, with all tables and relations implemented.

--- SysDevProject/app/src/main/java/com/example/sysdevproject/DatabaseHelper.java
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/DatabaseHelper.java
@@ -11,19 +11,55 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper{
 
     private static final String DB_NAME = "Restaurant.db";
-    
+    private static final String TABLE_ITEMS = "Item";
+    private static final String TABLE_CART = "Cart";
+    private static final String TABLE_ORDERS = "Orders";
+    private static final String TABLE_EMPLOYEE = "Employee";
+    private static final String TABLE_CUSTOMER = "Customer";
+
     public DatabaseHelper(@Nullable Context context) {
         super(context, DB_NAME, null, 1);
     }
 
 
+    private static final String CREATE_TABLE_ITEMS =
+            "create table Item ( item_id Integer primary key autoincrement, name varchar(64), description text, " +
+                    "price double, is_drink Integer, available Integer);";
+
+    private static final String CREATE_TABLE_EMPLOYEE =
+            "create table Employee ( employee_id Integer primary key autoincrement, name varchar(16));";
+
+    private static final String CREATE_TABLE_CUSTOMER =
+            "create table Customer ( customer_id Integer primary key autoincrement, isOver18 Integer, payment_method varchar(16));";
+
+    private static final String CREATE_TABLE_ORDERS =
+            "create table Orders ( order_id Integer primary key autoincrement, customer_id Integer, total_price double, foreign key(customer_id) references Customer(customer_id));";
+
+    private static final String CREATE_TABLE_CART =
+            "create table Cart ( order_id Integer, customer_id Integer, item_id Integer, quantity Integer, " +
+                    "foreign key(order_id) references Orders(order_id), " +
+                    "foreign key(customer_id) references Customer(customer_id), " +
+                    "foreign key(item_id) references Item(item_id), " +
+                    " PRIMARY KEY (customer_id,item_id));";
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL(CREATE_TABLE_ITEMS);
+        db.execSQL(CREATE_TABLE_EMPLOYEE);
+        db.execSQL(CREATE_TABLE_CUSTOMER);
+        db.execSQL(CREATE_TABLE_ORDERS);
+        db.execSQL(CREATE_TABLE_CART);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEMS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EMPLOYEE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CUSTOMER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ORDERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CART);
+        onCreate(db);
     }
 }
