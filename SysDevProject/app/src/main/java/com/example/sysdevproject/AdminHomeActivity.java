3c7614011efc6d9b228a782357abdc3273aa3905
Jamie <lowjamie8@gmail.com>
2021-05-03 19:03:18 -0400

    preparing admin home functions

--- SysDevProject/app/src/main/java/com/example/sysdevproject/AdminHomeActivity.java
+++ SysDevProject/app/src/main/java/com/example/sysdevproject/AdminHomeActivity.java
@@ -2,13 +2,52 @@ package com.example.sysdevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AdminHomeActivity extends AppCompatActivity {
+
+    Button add, delete, update, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        add = findViewById(R.id.addItem);
+        delete = findViewById(R.id.deleteItem);
+        update = findViewById(R.id.itemAvailability);
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminHomeActivity.this, MainActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add an new item to database
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //delete item from database
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update availability of an item
+            }
+        });
     }
 }
